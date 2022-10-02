ae1d54f6e115e3670296a794a590bc0e875992a7
Steve Cresswell <229672+cressie176@users.noreply.github.com>
2022-10-03 00:27:03 +0100

    Externalise GherkishFeatureParser expected results

--- test/GherkishFeatureParser.test.js
+++ test/GherkishFeatureParser.test.js
@@ -10,97 +10,19 @@ describe('FeatureParser', () => {
   it('should parse English by default', () => {
     const file = readFeatureFile('buck-rogers-season-one.feature');
     const parser = new GherkishFeatureParser();
-    const feature = parser.parse(file);
+    const actual = parser.parse(file);
+    const expected = readJsonFile('buck-rogers-season-one.json');
 
-    deq(feature, {
-      annotations: [
-        { name: 'only', value: true },
-        { name: 'library', value: 'Buck Rogers Common' },
-        { name: 'library', value: 'Buck Rogers Season One' },
-      ],
-      title: 'Buck Rogers - Season One',
-      description: undefined,
-      background: {
-        annotations: [
-          { name: 'timeout', value: '5000' },
-        ],
-        title: 'Introduction',
-        description: undefined,
-        steps: [
-          {
-            annotations: [],
-            text: 'Given the year is 1987',
-            generalised: 'Given the year is 1987',
-            docString: '',
-          },
-          {
-            annotations: [],
-            text: 'And NASA launches the last of America\'s deep space probes',
-            generalised: 'And NASA launches the last of America\'s deep space probes',
-            docString: '',
-          },
-          {
-            annotations: [
-              { name: 'skip', value: true },
-            ],
-            text: 'When a freak mishap occurs',
-            generalised: 'When a freak mishap occurs',
-            docString: '',
-          },
-          {
-            annotations: [],
-            text: 'Then Buck Rogers is blown out of his trajectory into an orbit which freezes his life support systems',
-            generalised: 'Then Buck Rogers is blown out of his trajectory into an orbit which freezes his life support systems',
-            docString: '',
-          },
-          {
-            annotations: [],
-            text: 'And returns Buck Rogers to Earth 500 years later',
-            generalised: 'And returns Buck Rogers to Earth 500 years later',
-            docString: '',
-          },
-        ],
-      },
-      scenarios: [
-        {
-          title: 'Awakening',
-          description: undefined,
-          annotations: [
-            { name: 'skip', value: true },
-          ],
-          steps: [
-            {
-              annotations: [],
-              text: 'Given the Draconians have planted a homing beacon aboard Buck\'s shuttle',
-              generalised: 'Given the Draconians have planted a homing beacon aboard Buck\'s shuttle',
-              docString: '',
-            },
-            {
-              annotations: [],
-              text: 'When Buck arrives on Earth',
-              generalised: 'When Buck arrives on Earth',
-              docString: '',
-            },
-            {
-              annotations: [],
-              text: 'Then he must adjust to the 25th century',
-              generalised: 'Then he must adjust to the 25th century',
-              docString: '',
-            },
-            {
-              annotations: [],
-              text: 'And convince the Earth Defense Directorate that the Draconians are secretly planning to conquer them',
-              generalised: 'And convince the Earth Defense Directorate that the Draconians are secretly planning to conquer them',
-              docString: '',
-            },
-          ],
-        },
-      ],
-    });
+    deq(actual, expected);
   });
 });
 
 function readFeatureFile(filename) {
   const filepath = path.join('test', 'specifications', 'en', filename);
   return fs.readFileSync(filepath, 'utf-8');
+}
+
+function readJsonFile(filename) {
+  const file = readFeatureFile(filename);
+  return JSON.parse(file);
 }
