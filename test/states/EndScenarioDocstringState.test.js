85ffa3d9f9e1f4b391b8715811850644e86b2a95
Steve Cresswell <229672+cressie176@users.noreply.github.com>
2022-11-18 22:58:31 +0000

    Add missing tests

---
+++ test/states/EndScenarioDocstringState.test.js
@@ -0,0 +1,97 @@
+import { strictEqual as eq, deepStrictEqual as deq, throws } from 'node:assert';
+import zunit from 'zunit';
+import { FeatureBuilder, StateMachine, States, Events } from '../../lib/index.js';
+import StubSession from '../stubs/StubSession.js';
+import StateMachineTestBuilder from './StateMachineTestBuilder.js';
+
+const { describe, it, xdescribe, xit, odescribe, oit, before, beforeEach, after, afterEach } = zunit;
+
+describe('EndScenarioDocstringState', () => {
+
+  const testBuilder = new StateMachineTestBuilder().beforeEach(() => {
+    const featureBuilder = new FeatureBuilder()
+      .createFeature({ title: 'Meh' })
+      .createBackground({ title: 'Meh' })
+      .createStep({ text: 'First step' });
+
+    const session = new StubSession();
+
+    const machine = new StateMachine({ featureBuilder, session })
+      .toStubState()
+      .checkpoint()
+      .toEndScenarioDocstringState();
+
+    testBuilder.featureBuilder = featureBuilder;
+    testBuilder.machine = machine;
+    testBuilder.expectedEvents = [
+      Events.AnnotationEvent,
+      Events.BlankLineEvent,
+      Events.BlockCommentDelimiterEvent,
+      Events.EndEvent,
+      Events.ExampleTableEvent,
+      Events.ScenarioEvent,
+      Events.SingleLineCommentEvent,
+      Events.StepEvent,
+    ];
+  });
+
+  testBuilder.interpreting('@foo=bar')
+    .shouldTransitionTo(States.StubState)
+    .shouldDispatch(Events.AnnotationEvent, (context) => {
+      eq(context.data.name, 'foo');
+      eq(context.data.value, 'bar');
+    });
+
+  testBuilder.interpreting('Background:')
+    .shouldBeUnexpected('a background');
+
+  testBuilder.interpreting('Background: A background')
+    .shouldBeUnexpected('a background');
+
+  testBuilder.interpreting('')
+    .shouldTransitionTo(States.StubState);
+
+  testBuilder.interpreting('Where:')
+    .shouldTransitionTo(States.StubState);
+
+  testBuilder.interpreting('---')
+    .shouldBeUnexpected('the start of an explicit docstring');
+
+  testBuilder.interpreting('\u0000')
+    .shouldTransitionTo(States.StubState);
+
+  testBuilder.interpreting('Feature:')
+    .shouldBeUnexpected('a feature');
+
+  testBuilder.interpreting('Feature: A feature')
+    .shouldBeUnexpected('a feature');
+
+  testBuilder.interpreting('Scenario:')
+    .shouldTransitionTo(States.StubState)
+    .shouldDispatch(Events.ScenarioEvent, (context) => {
+      eq(context.data.title, '');
+    });
+
+  testBuilder.interpreting('Scenario: A scenario')
+    .shouldTransitionTo(States.StubState)
+    .shouldDispatch(Events.ScenarioEvent, (context) => {
+      eq(context.data.title, 'A scenario');
+    });
+
+  testBuilder.interpreting('# some comment')
+    .shouldTransitionTo(States.StubState);
+
+  testBuilder.interpreting('###')
+    .shouldTransitionTo(States.StubState)
+    .shouldDispatch(Events.BlockCommentDelimiterEvent);
+
+  testBuilder.interpreting('some text')
+    .shouldTransitionTo(States.StubState)
+    .shouldDispatch(Events.StepEvent, (context) => {
+      eq(context.data.text, 'some text');
+    });
+
+  testBuilder.interpreting('   some text')
+    .shouldBeUnexpected('the start of an implicit docstring');
+
+});
