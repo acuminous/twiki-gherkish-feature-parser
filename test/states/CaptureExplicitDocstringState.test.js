19b2197c4d7d4be9a7f6060885e893fd28651df9
Steve Cresswell <229672+cressie176@users.noreply.github.com>
2022-11-18 20:55:08 +0000

    Refactor state machine

--- test/states/CaptureExplicitDocstringState.test.js
+++ test/states/CaptureExplicitDocstringState.test.js
@@ -1,193 +1,97 @@
 import { strictEqual as eq, deepStrictEqual as deq, throws } from 'node:assert';
-import os from 'node:os';
 import zunit from 'zunit';
-import { FeatureBuilder, StateMachine } from '../../lib/index.js';
+import { FeatureBuilder, StateMachine, States, Events } from '../../lib/index.js';
 import StubSession from '../stubs/StubSession.js';
+import StateMachineTestBuilder from './StateMachineTestBuilder.js';
 
 const { describe, it, xdescribe, xit, odescribe, oit, before, beforeEach, after, afterEach } = zunit;
 
 describe('CaptureExplicitDocstringState', () => {
-  let featureBuilder;
-  let machine;
-  const expectedEvents = [
-    ' - a docstring line',
-    ' - the end of an explicit docstring',
-  ].join('\n');
 
-  beforeEach(() => {
-    featureBuilder = new FeatureBuilder()
+  const testBuilder = new StateMachineTestBuilder().beforeEach(() => {
+    const featureBuilder = new FeatureBuilder()
       .createFeature({ title: 'Meh' })
-      .createScenario({ title: 'Meh' })
-      .createStep({ text: 'First step' })
-      .createDocstring({ text: 'some docstring' });
+      .createBackground({ title: 'Meh' })
+      .createStep({ text: 'Meh' });
 
-    const session = new StubSession({ docstring: { delimiter: '---' } });
+    const session = new StubSession()
+      .beginExplicitDocstring('---', 0);
 
-    machine = new StateMachine({ featureBuilder, session })
-      .toDeclareFeatureState()
-      .checkpoint()
-      .toDeclareScenarioState()
-      .toCaptureScenarioDetailsState()
-      .checkpoint()
-      .toCaptureScenarioStepState()
-      .toBeginExplicitDocstringState()
+    const machine = new StateMachine({ featureBuilder, session })
+      .alias(States.EndFeatureBackgroundDocstringState)
       .toCaptureExplicitDocstringState();
+
+    testBuilder.featureBuilder = featureBuilder;
+    testBuilder.machine = machine;
+    testBuilder.expectedEvents = [
+      Events.DocstringTextEvent,
+      Events.ExplicitDocstringStopEvent,
+    ];
   });
 
-  describe('Annotations', () => {
-    it('should not cause a state transition', () => {
-      interpret('@foo = bar');
-      eq(machine.state, 'CaptureExplicitDocstringState');
+  testBuilder.interpreting('@foo=bar')
+    .shouldNotTransition()
+    .shouldCapture('docstring', (feature) => {
+      eq(feature.background.steps[0].docstring, '@foo=bar');
     });
 
-    it('should be captured', () => {
-      interpret('@foo = bar');
-
-      const exported = featureBuilder.build();
-      eq(exported.scenarios[0].steps[0].docstring, ['some docstring', '@foo = bar'].join(os.EOL));
-    });
-  });
-
-  describe('Backgrounds', () => {
-    it('should not cause a state transition', () => {
-      interpret('Background: foo');
-      eq(machine.state, 'CaptureExplicitDocstringState');
+  testBuilder.interpreting('Background:')
+    .shouldNotTransition()
+    .shouldCapture('docstring', (feature) => {
+      eq(feature.background.steps[0].docstring, 'Background:');
     });
 
-    it('should be captured', () => {
-      interpret('Background: foo');
-
-      const exported = featureBuilder.build();
-      eq(exported.scenarios[0].steps[0].docstring, ['some docstring', 'Background: foo'].join(os.EOL));
-    });
-  });
-
-  describe('Blank lines', () => {
-    it('should not cause a state transition', () => {
-      interpret('');
-      eq(machine.state, 'CaptureExplicitDocstringState');
+  testBuilder.interpreting('')
+    .shouldNotTransition()
+    .shouldCapture('docstring', (feature) => {
+      eq(feature.background.steps[0].docstring, '');
     });
 
-    it('should be captured', () => {
-      interpret('');
-
-      const exported = featureBuilder.build();
-      eq(exported.scenarios[0].steps[0].docstring, ['some docstring', ''].join(os.EOL));
-    });
-  });
-
-  describe('Example tables', () => {
-    it('should not cause a state transition', () => {
-      interpret('Where:');
-      eq(machine.state, 'CaptureExplicitDocstringState');
+  testBuilder.interpreting('Where:')
+    .shouldNotTransition()
+    .shouldCapture('docstring', (feature) => {
+      eq(feature.background.steps[0].docstring, 'Where:');
     });
 
-    it('should be captured', () => {
-      interpret('Where:');
+  testBuilder.interpreting('---')
+    .shouldTransitionTo(States.EndFeatureBackgroundDocstringState);
 
-      const exported = featureBuilder.build();
-      eq(exported.scenarios[0].steps[0].docstring, ['some docstring', 'Where:'].join(os.EOL));
-    });
-  });
+  testBuilder.interpreting('\u0000')
+    .shouldBeUnexpected('the end of the feature');
 
-  describe('Explicit docstring delimiters', () => {
-    it('should cause a transition to CaptureScenarioDetailsState', () => {
-      interpret('---');
-      eq(machine.state, 'CaptureScenarioDetailsState');
-    });
-  });
-
-  describe('Implicit docstring delimiters', () => {
-    it('should not cause a state transition', () => {
-      interpret('   some more docstring');
-      eq(machine.state, 'CaptureExplicitDocstringState');
+  testBuilder.interpreting('Feature:')
+    .shouldNotTransition()
+    .shouldCapture('docstring', (feature) => {
+      eq(feature.background.steps[0].docstring, 'Feature:');
     });
 
-    it('should be captured', () => {
-      interpret('   some more docstring');
-
-      const exported = featureBuilder.build();
-      eq(exported.scenarios[0].steps[0].docstring, ['some docstring', '   some more docstring'].join(os.EOL));
-    });
-  });
-
-  describe('End of file', () => {
-    it('should be unexpected', () => {
-      throws(() => interpret('\u0000'), { message: `I did not expect the end of the feature at index.js:1\nInstead, I expected one of:\n${expectedEvents}\n` });
-    });
-  });
-
-  describe('Features', () => {
-    it('should not cause a state transition', () => {
-      interpret('Feature: foo');
-      eq(machine.state, 'CaptureExplicitDocstringState');
+  testBuilder.interpreting('Scenario:')
+    .shouldNotTransition()
+    .shouldCapture('docstring', (feature) => {
+      eq(feature.background.steps[0].docstring, 'Scenario:');
     });
 
-    it('should be captured', () => {
-      interpret('Feature: foo');
-
-      const exported = featureBuilder.build();
-      eq(exported.scenarios[0].steps[0].docstring, ['some docstring', 'Feature: foo'].join(os.EOL));
-    });
-  });
-
-  describe('Block comment delimiters', () => {
-    it('should not cause a state transition', () => {
-      interpret('###');
-      eq(machine.state, 'CaptureExplicitDocstringState');
+  testBuilder.interpreting('# some comment')
+    .shouldNotTransition()
+    .shouldCapture('docstring', (feature) => {
+      eq(feature.background.steps[0].docstring, '# some comment');
     });
 
-    it('should be captured', () => {
-      interpret('###');
-
-      const exported = featureBuilder.build();
-      eq(exported.scenarios[0].steps[0].docstring, ['some docstring', '###'].join(os.EOL));
-    });
-  });
-
-  describe('Single line comments', () => {
-    it('should not cause a state transition', () => {
-      interpret('# A comment');
-      eq(machine.state, 'CaptureExplicitDocstringState');
+  testBuilder.interpreting('###')
+    .shouldNotTransition()
+    .shouldCapture('docstring', (feature) => {
+      eq(feature.background.steps[0].docstring, '###');
     });
 
-    it('should be captured', () => {
-      interpret('# A comment');
-
-      const exported = featureBuilder.build();
-      eq(exported.scenarios[0].steps[0].docstring, ['some docstring', '# A comment'].join(os.EOL));
-    });
-  });
-
-  describe('Scenarios', () => {
-    it('should not cause a state transition', () => {
-      interpret('Scenario: foo');
-      eq(machine.state, 'CaptureExplicitDocstringState');
+  testBuilder.interpreting('some text')
+    .shouldNotTransition()
+    .shouldCapture('docstring', (feature) => {
+      eq(feature.background.steps[0].docstring, 'some text');
     });
 
-    it('should be captured', () => {
-      interpret('Scenario: foo');
-
-      const exported = featureBuilder.build();
-      eq(exported.scenarios[0].steps[0].docstring, ['some docstring', 'Scenario: foo'].join(os.EOL));
+  testBuilder.interpreting('   some text')
+    .shouldNotTransition()
+    .shouldCapture('docstring', (feature) => {
+      eq(feature.background.steps[0].docstring, '   some text');
     });
-  });
-
-  describe('Lines of text', () => {
-    it('should not cause a state transition', () => {
-      interpret('some more text');
-      eq(machine.state, 'CaptureExplicitDocstringState');
-    });
-
-    it('should be captured', () => {
-      interpret('some more text');
-
-      const exported = featureBuilder.build();
-      eq(exported.scenarios[0].steps[0].docstring, ['some docstring', 'some more text'].join(os.EOL));
-    });
-  });
-
-  function interpret(line, number = 1) {
-    machine.interpret({ line, number });
-  }
 });
